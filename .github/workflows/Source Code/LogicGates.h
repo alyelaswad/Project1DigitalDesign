5d17c2cc8677a0df63b50d2e63be36a297471865
Aly Elaswad <124714695+alyelaswad@users.noreply.github.com>
2024-03-07 19:59:18 +0200

    Upload Source Code File: LogicGates.h

---
+++ .github/workflows/Source Code/LogicGates.h
@@ -0,0 +1,17 @@
+#ifndef LOGICGATES_H
+#define LOGICGATES_H
+class LogicGates
+{
+public:
+    bool AND(bool, bool);
+
+    bool OR(bool, bool);
+
+    bool NOT(bool);
+
+    bool NAND(bool, bool);
+
+    bool NOR(bool, bool);
+};
+
+#endif
