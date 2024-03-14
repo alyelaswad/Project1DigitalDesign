a75855b73fac583e61d66980232a1c17f7022bad
IsmailSabryy <124688766+IsmailSabryy@users.noreply.github.com>
2024-03-14 02:29:36 +0200

    Update libfilereader.h

--- .github/workflows/Source Code/libfilereader.h
+++ .github/workflows/Source Code/libfilereader.h
@@ -4,14 +4,38 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <stack>
+#include <map>
+#include <cstdarg>
 #include "LogicGates.h" 
 using namespace std;
 struct gate {
     string name;
     int inputnums;
-    string expression;
     int delayofgate;
+    LogicGates g;
+    bool output(bool A, bool B) {
+        if (name == "NAND2") {
+            return g.NAND(A, B);
+        }
+        else if (name == "NOT") {
+            return g.NOT(A);
+        }
+        else if (name == "AND2") {
+            return g.AND(A, B);
+        }
+        else if (name == "OR2") {
+            return g.OR(A, B);
+        }
+        else if (name == "NOR2") {
+            return g.NOR(A, B);
+        }
+
+    }
+   
+    vector <string> inputs;
 };
+
 
 class libfilereader {
 private:
@@ -19,6 +43,14 @@ private:
 
 public:
    void accesslibfile(string);
- /* void readvectorgate();*/
+  void readvectorgate();
+  bool getouput(bool A, bool B) {
+
+      return gatesdict[2].output(A, B);
+  }
+  bool getouput(bool A) {
+
+      return gatesdict[0].output(A,A);
+  }
 };
 
