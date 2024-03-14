0909ae5f0337cb983f9d46dea66c76139a19d3ca
Aly Elaswad <124714695+alyelaswad@users.noreply.github.com>
2024-03-14 22:01:15 +0200

    Update LogicGates.cpp

--- .github/workflows/Source Code/LogicGates.cpp
+++ .github/workflows/Source Code/LogicGates.cpp
@@ -1,21 +1,21 @@
 #include "LogicGates.h"
 bool LogicGates::AND(bool A, bool B)
 {
-    return A && B;
+    return A && B; // Using the AND method embedded in the c++ language
 }
 bool LogicGates::OR(bool A, bool B)
 {
-    return A || B;
+    return A || B; // Using the OR method embedded in the c++ language
 }
 bool LogicGates::NOT(bool A)
 {
-    return !A;
+    return !A; // Using the NOT method embedded in the c++ language
 }
 bool LogicGates::NAND(bool A, bool B)
 {
-    return NOT(AND(A, B));
+    return NOT(AND(A, B)); // Calls NOT after AND gates to achieve NAND
 }
 bool LogicGates::NOR(bool A, bool B)
 {
-    return NOT(OR(A, B));
+    return NOT(OR(A, B));  // Calls NOT after OR gates to achieve NOR
 }
