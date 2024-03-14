c66e93b89b0af0e5f6ef2276908d94bb772198bd
Islamauc <144138231+Islamauc@users.noreply.github.com>
2024-03-14 23:00:32 +0200

    Update cir_reader.h

--- .github/workflows/Source Code/cir_reader.h
+++ .github/workflows/Source Code/cir_reader.h
@@ -7,10 +7,11 @@ using namespace std;
 class circuit
 {
 private:
-vector <char> inputs;
-vector <gate*> lib_gates;
+vector <char> inputs; // A vector that contains inputs as characters
+vector <gate*> lib_gates; // A vector that stores pointers to gates
 public:
-circuit(libfilereader lib_file);
-void access_cir_file(std::string pathname);
+circuit(libfilereader lib_file); // A constructor that recieves a lib_file object to store the vector of lib_gates
+void access_cir_file(std::string pathname); // A function that reads the .cir file and stores the gates used in the circuit in the cir_gates 
+                                                vector
 
 };
