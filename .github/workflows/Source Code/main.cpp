6d38e8aa1b3635a622552487aa29b4752d9e093b
IsmailSabryy <124688766+IsmailSabryy@users.noreply.github.com>
2024-03-23 15:59:46 +0200

    Update and rename dummyrpoject.cpp to main.cpp

--- .github/workflows/Source Code/main.cpp
+++ .github/workflows/Source Code/main.cpp
@@ -5,7 +5,6 @@ int main()
 {
     CircuitReader circuit;
     circuit.accessLibFile("DD1.lib");
-    // circuit.readvectorgate();
     circuit.accessCirFile("circ5.cir");
     circuit.accessStimFile("circ_2Var.stim");
 
