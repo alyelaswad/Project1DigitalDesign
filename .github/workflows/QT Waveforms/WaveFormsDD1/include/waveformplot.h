f15e01131fc27e504c122857b41ac086eb670243
Aly Elaswad <124714695+alyelaswad@users.noreply.github.com>
2024-03-23 03:48:58 +0200

    Create waveformplot.h

---
+++ .github/workflows/QT Waveforms/WaveFormsDD1/include/waveformplot.h
@@ -0,0 +1,16 @@
+#ifndef WAVEFORMPLOT_H
+#define WAVEFORMPLOT_H
+
+#include <QMainWindow>
+
+class waveformplot : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit waveformplot(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // WAVEFORMPLOT_H
